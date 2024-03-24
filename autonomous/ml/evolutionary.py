befd80dae5d3421570e5056460ddd4220750ee04
mitya <dmitya26@gmail.com>
2024-03-24 11:05:03 -0700

    test commit

---
+++ autonomous/ml/evolutionary.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.insert(0, "/Users/mitya/desktop/speed/racecar/ipc")
+import file
+
+"""
+a = f.read() # literally just run this command and it'll handle the rest for you
+print(a)
+"""
+
+# read an is-crashed value to check if the car has crashed or not.
+#
